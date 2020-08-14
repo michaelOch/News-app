8b61560dc8baa3d7717a222dd5738565dcaf4b37
michaelOch <ochuenwikechukwudi@hotmail.com>
2020-08-13 19:48:36 -0700

    Updates

---
+++ precache-manifest.31db1973ec48e1884ef7c8a675ac1605.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e459b8c3c0b73e0115bb77eab1889d7c",
+    "url": "/News-app/index.html"
+  },
+  {
+    "revision": "1fe10fbb4953ec2dbd4d",
+    "url": "/News-app/static/css/main.c0eb4bda.chunk.css"
+  },
+  {
+    "revision": "e949b07d0edd8a354356",
+    "url": "/News-app/static/js/2.ea783f25.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/News-app/static/js/2.ea783f25.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1fe10fbb4953ec2dbd4d",
+    "url": "/News-app/static/js/main.d6315aed.chunk.js"
+  },
+  {
+    "revision": "c3324ac2ea5d2ca62df3",
+    "url": "/News-app/static/js/runtime-main.54baa3c5.js"
+  },
+  {
+    "revision": "7a361cacf21627565ae66566a7d56536",
+    "url": "/News-app/static/media/cnbc.7a361cac.png"
+  },
+  {
+    "revision": "1b29ecb970fb75b0257153c2f23d0bde",
+    "url": "/News-app/static/media/cnn.1b29ecb9.png"
+  },
+  {
+    "revision": "f9b0f5148b56f51ee26b2210dbb88eca",
+    "url": "/News-app/static/media/daily.f9b0f514.png"
+  },
+  {
+    "revision": "318a765f1a543601c84939abdc6e0614",
+    "url": "/News-app/static/media/echo.318a765f.png"
+  },
+  {
+    "revision": "f60d22c950b96b6b7dae30e3bf255d13",
+    "url": "/News-app/static/media/guardian.f60d22c9.jpg"
+  },
+  {
+    "revision": "5260d366c995bce983b62e529ddab587",
+    "url": "/News-app/static/media/sky.5260d366.png"
+  }
+]);
